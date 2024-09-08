d54b9951b45744b0ab488c5a48b20ac34506246b
Aliza <alizashahsavari@gmail.com>
2024-09-09 01:31:57 +0330

    Initial commit

---
+++ Spring Boot/aliza-springBoot/src/main/kotlin/com/aliza/aliza_springBoot/AlizaSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.aliza.aliza_springBoot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlizaSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlizaSpringBootApplication>(*args)
+}

7ae698c2e878971eecc5bd1c9a15f35f757c7388
Aliza <alizashahsavari@gmail.com>
2024-09-09 23:03:52 +0330

    Initial commit

---
+++ Spring Boot/alizaShop/src/main/kotlin/com/aliza/alizaShop/AlizaShopApplication.kt
@@ -0,0 +1,11 @@
+package com.aliza.alizaShop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlizaShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlizaShopApplication>(*args)
+}
